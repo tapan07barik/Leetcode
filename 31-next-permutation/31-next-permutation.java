45e286d54b0cda21270fe8887d94bbe78af9892d
Tapan Kumar Barik <110212137+tapan07barik@users.noreply.github.com>
2022-07-31 17:27:19 +0530

    Time: 2 ms (27.16%), Space: 43.9 MB (39.82%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -4,18 +4,23 @@ class Solution {
         int n=nums.length;
         int i=n-2;
         
-        //finding the incremental number
+        //finding the number thst is greater than the next number
         while(i>=0 && nums[i]>=nums[i+1]){
             i--;
         }
+        // if the num is sorted i will be 0;
         if(i>=0){
             
             int j=n-1;
+            ///finding the  number just greater than nums[i]
             while(j>=0 && nums[j]<=nums[i]){
                 j--;
             }
+            //
             swap(nums, i, j);
         }
+        
+        
         reverse(nums,i+1, n-1);
     }
     
